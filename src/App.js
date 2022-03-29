9b7d2241482edb75d6a02ea7bf1d9e790c231253
Dino-Nguyen <xiao.dinnie@gmail.com>
2022-03-29 13:29:56 +0700

    1 commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+
 
 function App() {
+ const ColorList = [
+   ["red", "none", "none"],
+   ["none","yellow","none"],
+   ["none","none","green"],
+ ]
+
+
+let [light, setLight] = useState(ColorList[0])
+let  [Red, Yellow, Green] = light
+const changeColor = () => {
+  
+ if (light != ColorList[1] && light != ColorList[2]) {
+      light = ColorList[1]
+      
+ 
+ } 
+ else if (light != ColorList[0] && light != ColorList[2] ){
+     light = ColorList[2]
+     
+ 
+ }
+ else  if (light != ColorList[0] && light != ColorList[1]) {
+     light = ColorList[0]
+     
+ 
+  }
+  console.log(light)
+  // setLight(lights)
+ 
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <button onClick={changeColor}>Next</button>
+      </div>
+      <div>
+       <div className='light'  style={{background: Red}}></div>
+       <div className='light'  style={{background: Yellow}}></div>
+       <div className='light'  style={{background: Green}}></div>
+       </div>
     </div>
   );
 }
